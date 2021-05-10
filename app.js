d1e69126e5a6fbbe715e3bff19270684087d5cae
相川悠太 <kawa@aikawayuutanoMacBook-Air.local>
2021-05-10 14:19:17 +0900

    [Learn] 3.1

--- app.js
+++ app.js
@@ -1,68 +1,20 @@
-// 1章
-// var hello = "Hello World";
-// alert(hello);
-
-
-// 2章
-// alert(4 + 3);
-
-// var str1 = 'Hello';
-// var str2 = 'World!!!';
-// alert(str1 + str2);
-
-// var orange = 100;
-// var apple = 120;
-
-// if(orange < apple){
-//   alert("みかんの値段がりんごより安い");
-// } else if(orange == apple){
-//   alert("みかんとりんごが同じ値段");
-// } else{
-//   alert("みかんの値段がりんごより高い");
-// }
-
-// var max = 100;
-// var num = 1;
-// var count = 0;
-
-// while(num < max){
-//   num = num*2;
-//   count = count + 1;
-// }
-
-// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
-
-// do{
-//   最低１回は行われる処理
-// }while(条件式);
-
-// var i;
-// var num = 0;
-
-// for(i=1; i<11; i++){
-//   num = num + i;
-// }
-
-// alert('1から10まで足し算した結果は' + num + 'です');
-
-
 // ３章
 // メイン部分
-// var alertString;
-// alertString = addString("WebCamp");
+var alertString;
+alertString = addString("WebCamp");
 
 // 作成した関数を呼び出す
-// alert(alertString);
+alert(alertString);
 
 // 作成した関数
-//function addString(strA){
-//   var addStr = "Hello" + strA;
-//   return addStr;
-// }
+function addString(strA){
+var addStr = "Hello" + strA;
+return addStr;
+}
 
 // 入力ダイアログ
-// var promptStr = prompt('何か好きな文字を入力してください。');
-// alert(promptStr);
+var promptStr = prompt('何か好きな文字を入力してください。');
+alert(promptStr);
 
 // じゃんけんの手を入力してもらうプロンプト欄を生成
 var user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
@@ -89,7 +41,7 @@ function getJShand(){
 }
 // ユーザの手とJavaScriptのじゃんけんの手を比べる関数
 function WinLose(user,js){
-  var WinLoseStr,
+  var WinLoseStr;
 
   if(user == "グー"){
     if(js == "グー"){
