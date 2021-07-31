cb0aad0d9124d05e0fe4b1eb1cfdd70e8585f16a
ykawa727 <y.aikawa727@gmail.com>
2021-07-31 20:14:35 +0900

    learned 2

--- app.js
+++ app.js
@@ -1,1 +1,56 @@
-alert('Hello World');
+// let hello = 'Hello World';
+// alert(hello);
+
+// let int1 = 1;
+// alert(int1);
+
+// alert(4 + 3);
+
+// alert('Hello' + 'World');
+
+// let str1 = 'Hello'
+// let str2 = 'World!!'
+// alert(str1 + str2);
+
+// let orange = 100;
+// let apple = 120;
+
+// if(orange < apple){
+//   alert('みかんの値段がリンゴより安い');
+// } else if(orange == apple){
+//   alert('みかんとりんごが同じ値段');
+// } else{
+//   alert('みかんの値段がリンゴより高い');
+// }
+
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while(num < max){
+//   num *= 2;
+//   count += 1;
+// }
+
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+// let i;
+// let num = 0;
+
+// for(i = 1; i < 11; i++){
+//   num = num + i;
+// }
+
+// alert('1から10まで足し算した結果は' + num + 'です');
+
+let int1 = 2 + 7;
+alert('2+7=' + int1);
+
+let int2 = 7 - 2;
+alert('7-2=' + int2);
+
+let int3 = 2 * 7;
+alert('2*7=' + int3);
+
+let int4 = 7 / 2;
+alert('7/2=' + int4);
